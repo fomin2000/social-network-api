28e9fbcb1d7ed7f8ababe1d8c4ac8355558b9739
Pasha Fomin <pashafomin@Pashas-MacBook-Pro.local>
2023-03-06 17:37:59 -0800

    added more models and data for seeds

---
+++ utils/data.js
@@ -0,0 +1,75 @@
+const userData = [
+    {username: 'Nathan', email: 'nathan@gmail.com'},
+    {username: 'Reese', email: 'reese@gmail.com'},
+    {username: 'Sonny', email: 'sonny@gmail.com'},
+    {username: 'Jemma', email: 'jemma@gmail.com'},
+    {username: 'Derrick', email: 'derrick@gmail.com'},
+    {username: 'Katalina', email: 'katalina@gmail.com'},
+    {username: 'Remi', email: 'remi@gmail.com'},
+    {username: 'Loretta', email: 'loretta@gmail.com'},
+    {username: 'Aaron', email: 'aaron@gmail.com'},
+]
+
+
+const thoughData = [
+    {
+        thoughtText:
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        username: 'Nathan'
+    }, 
+    {
+        thoughtText:
+            'aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        username: 'Reese'
+    },
+    {
+        thoughtText:
+            'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        username: 'Sonny'
+    },
+    {
+        thoughtText:
+            'Irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        username: 'Jemma'
+    },
+    {
+        thoughtText:
+            'Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        username: 'Derrick'
+    },
+    {
+        thoughtText:
+            'amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        username: 'Katalina'
+    },
+    {
+        thoughtText:
+            'Ipsum lorem dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        username: 'Remi'
+    },
+    {
+        thoughtText:
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        username: 'Loretta'
+    },
+    {
+        thoughtText:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam amet, est magni doloribus deserunt in molestias non sit accusantium sint, aliquam cum voluptatibus saepe consectetur! Nulla consequatur perferendis esse nesciunt.',
+        username: 'Aaron'
+    }
+]
+
+
+const reactionData = [
+    { username: 'Nathan', reactionBody: 'awesome' },
+    { username: 'Resse', reactionBody: 'great' },
+    { username: 'Sonny', reactionBody: 'nice' },
+    { username: 'Jemma', reactionBody: 'Love it' },
+    { username: 'Derrick', reactionBody: 'ehhh' },
+    { username: 'Katalina', reactionBody: 'Wow' },
+    { username: 'Rema', reactionBody: 'nteresting' },
+    { username: 'Loretta', reactionBody: 'cool' },
+    { username: 'Aaron', reactionBody: 'Nah' },
+  ]
+  
+  module.exports = { userData, thoughtData, reactionData }
